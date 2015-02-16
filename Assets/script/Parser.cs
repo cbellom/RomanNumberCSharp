76b891bb22d37e20b1f8b98784dba20d560e5ccc
cbellom <cbellom@unal.edu.co>
2015-02-16 11:28:48 -0500

    init

---
+++ Assets/script/Parser.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parser : MonoBehaviour {
+	private int[] values ={ 1000, 900, 500, 400, 100, 90,50, 40, 10, 9, 5, 4, 1 };
+	private string[] romanNumbers ={ "M", "CM", "D", "CD", "C","XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+	public string getRomanNumberFromInt(int number){
+		string romanNumber = "";
+		int index = 0;
+
+		while (number > 0){
+			while (number >= values[index]){
+				romanNumber += romanNumbers[index];
+				number = number - values[index];
+			}
+			index++;
+		}
+		return romanNumber;
+	}
+
+}
---
+++ Assets/script/PlusManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlusManager : MonoBehaviour {
+	public int firstNumber;
+	public int secondNumber;
+
+	private int result;	
+	private Parser parser;
+	private string message;
+
+	void Start () {
+		plusNumbersAndParser ();
+	}
+
+	public void plusNumbersAndParser(){
+		if (!validateNumber (firstNumber) || !validateNumber (secondNumber)) {
+			message = "Numero(s) invalido(s) debe(n) ser mayor que 0 y menor que 5000";
+		} else {
+			result = firstNumber + secondNumber;
+			
+			if(!validateNumber(result))
+				message = "La suma de los dos numeros debe ser mayor que 0 y menor que 5000";
+			else
+				message = "Numero Romano: " + parseIntToRomanNumber(result);
+		}
+		
+		print (message);
+	}
+
+	public string parseIntToRomanNumber(int number){
+		parser = new Parser ();
+		return parser.getRomanNumberFromInt (result);
+	}
+
+	public bool validateNumber(int number){
+		if (number <= 0 || number >= 5000)
+			return false;
+
+		return true;
+	}
+}
